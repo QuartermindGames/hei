7c212a4f54f21458bdc51e5107f54a36db542bad
hogsy <markelswo@gmail.com>
2017-02-09 00:05:45 +0000

    Fixed header.

--- platform/platform_image_wad.c
+++ platform/platform_image_wad.c
@@ -19,6 +19,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
+#include "platform_image.h"
+
 typedef struct WADHeader {
     PLchar ident[4];    // WAD2, 2DAW, IWAD, PWAD
 
