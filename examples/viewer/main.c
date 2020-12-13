e1467a16e88c4738d47bef5f2ae1faa684723b36
hogsy <markelswo@gmail.com>
2020-12-13 14:20:46 +0000

    Start cleaning up the viewer, so we can use it for testing the Vulkan renderer.

--- examples/viewer/main.c
+++ examples/viewer/main.c
@@ -25,13 +25,13 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org>
 */
 
-#include <PL/platform_math.h>
-#include <PL/platform_console.h>
 #include <PL/pl_graphics.h>
 #include <PL/pl_graphics_camera.h>
+#include <PL/platform_console.h>
+#include <PL/platform_filesystem.h>
+#include <PL/platform_math.h>
 #include <PL/platform_model.h>
 #include <PL/platform_package.h>
-#include <PL/platform_filesystem.h>
 
 #include <PL/pl_window.h>
 
@@ -39,63 +39,30 @@ For more information, please refer to <http://unlicense.org>
 
 #define TITLE "Model Viewer"
 
-#define VERSION_MAJOR   0
-#define VERSION_MINOR   3
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 3
 
-#define WIDTH   800
-#define HEIGHT  600
+#define WIDTH 800
+#define HEIGHT 600
 
-#define CENTER_X    WIDTH / 2
-#define CENTER_Y    HEIGHT / 2
+#define CENTER_X WIDTH / 2
+#define CENTER_Y HEIGHT / 2
 
 static PLCamera *mainCamera = NULL;
 static PLWindow *mainWindow = NULL;
 
 enum {
-    VIEW_MODE_LIT,
-    VIEW_MODE_WIREFRAME,
-    VIEW_MODE_POINTS,
-    VIEW_MODE_WEIGHTS,
-    VIEW_MODE_SKELETON
+	VIEW_MODE_LIT,
+	VIEW_MODE_WIREFRAME,
+	VIEW_MODE_POINTS,
+	VIEW_MODE_WEIGHTS,
+	VIEW_MODE_SKELETON
 };
 static unsigned int viewMode = VIEW_MODE_LIT;
 
-//////////////////////////////////////////
-
+static void ProcessKeyboard( void ) {
 #if 0
-
-SDL_Window *window = NULL;
-static void CreateWindow( void ) {
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 2 );
-
-    window = SDL_CreateWindow(
-        TITLE,
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        WIDTH, HEIGHT,
-        SDL_WINDOW_OPENGL
-    );
-    if( window == NULL ) {
-        PRINT_ERROR( "SDL2: %s\n", SDL_GetError() );
-    }
-
-    PLGraphicsContext *context = SDL_GL_CreateContext( window );
-    if( context == NULL ) {
-        PRINT_ERROR( "SDL2: %s\n", SDL_GetError() );
-    }
-
-    SDL_GL_MakeCurrent( window, context );
-    if( SDL_GL_SetSwapInterval( -1 ) == -1 ) {
-        SDL_GL_SetSwapInterval( 1 );
-    }
-
-    SDL_DisableScreenSaver();
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-
-static void ProcessKeyboard(void) {
-    const uint8_t *state = SDL_GetKeyboardState(NULL);
+	const uint8_t *state = SDL_GetKeyboardState(NULL);
     if(state[SDL_SCANCODE_1]) {
         view_mode = VIEW_MODE_LIT;
     } else if(state[SDL_SCANCODE_2]) {
@@ -152,241 +119,242 @@ static void ProcessKeyboard(void) {
     if( state[ SDL_SCANCODE_ESCAPE ] ) {
         exit( EXIT_SUCCESS );
     }
-}
-
 #endif
+}
 
 // loads a model in and then frees it
 static void TempModelLoad( const char *path, void *userData ) {
 	plUnused( userData );
 
-    PLModel *model = plLoadModel( path );
-    if( model != NULL ) {
-        plDestroyModel( model );
-    }
+	PLModel *model = plLoadModel( path );
+	if ( model != NULL ) {
+		plDestroyModel( model );
+	}
 }
 
 int main( int argc, char **argv ) {
-    PRINT( "\n " TITLE " : Version %d.%d (" __DATE__ ")\n", VERSION_MAJOR, VERSION_MINOR );
-    PRINT( " Developed by...\n" );
-    PRINT( "   Mark \"hogsy\" Sowden (http://talonbrave.info/)\n" );
-    PRINT( "\n" );
-    PRINT( " Usage:\n" );
-    PRINT( "  Left   - rotate model\n" );
-    PRINT( "  Right  - move model backward / forward\n" );
-    PRINT( "  Middle - move model up, down, left and right\n\n" );
-    PRINT( "  WASD   - move camera\n" );
-    PRINT( "\n-------------------------------------------------------------------------\n\n" );
+	PRINT( "\n " TITLE " : Version %d.%d (" __DATE__ ")\n", VERSION_MAJOR, VERSION_MINOR );
+	PRINT( " Developed by...\n" );
+	PRINT( "   Mark \"hogsy\" Sowden <hogsy@oldtimes-software.com>\n" );
+	PRINT( "\n" );
+	PRINT( " Usage:\n" );
+	PRINT( "  Left   - rotate model\n" );
+	PRINT( "  Right  - move model backward / forward\n" );
+	PRINT( "  Middle - move model up, down, left and right\n\n" );
+	PRINT( "  WASD   - move camera\n" );
+	PRINT( "\n-------------------------------------------------------------------------\n\n" );
 
-    plInitialize( argc, argv );
-    plSetupLogOutput( "./viewer.log" );
+	plInitialize( argc, argv );
+	plSetupLogOutput( "./viewer.log" );
 
-    plRegisterStandardPackageLoaders();
-    plRegisterStandardModelLoaders();
+	plRegisterStandardPackageLoaders();
+	plRegisterStandardModelLoaders( PL_MODEL_FILEFORMAT_ALL );
+	plRegisterStandardImageLoaders( PL_IMAGE_FILEFORMAT_ALL );
 
-    if( argc < 2 ) {
-        PRINT( " viewer -<optional mode> <model path>\n" );
-        PRINT( "  -smd    : write model out to an SMD\n" );
-        PRINT( "  -scan   : scans through a directory, must provide extension as follow-up argument to model path\n" );
-        return EXIT_SUCCESS;
-    }
+	if ( argc < 2 ) {
+		PRINT( " viewer -<optional mode> <model path>\n" );
+		PRINT( "  -smd    : write model out to an SMD\n" );
+		PRINT( "  -scan   : scans through a directory, must provide extension as follow-up argument to model path\n" );
+		return EXIT_SUCCESS;
+	}
 
-    char model_path[ PL_SYSTEM_MAX_PATH ] = { '\0' };
-    char model_extension[ 12 ] = { '\0' };
+	char model_path[ PL_SYSTEM_MAX_PATH ] = { '\0' };
+	char model_extension[ 12 ] = { '\0' };
 
-    bool extract_model = false;
-    bool scan_directory = false;
-    bool useMouseLook = false;
+	bool exportModel = false;
+	bool scanDirectory = false;
+	bool useMouseLook = false;
 
-    for( int i = 1; i < argc; ++i ) {
-        if( argv[ i ] == NULL || argv[ i ][ 0 ] == '\0' ) {
-            continue;
-        }
+	if ( plHasCommandLineArgument( "-smd" ) ) {
+		exportModel = true;
+	} else if ( plHasCommandLineArgument( "-scan" ) ) {
+		scanDirectory = true;
+	}
 
-        if( pl_strcasecmp( "-smd", argv[ i ] ) == 0 ) {
-            extract_model = true;
-        } else if( pl_strcasecmp( "-scan", argv[ i ] ) == 0 ) {
-            scan_directory = true;
-        } else if( argv[ i ][ 0 ] != '-' ) { // probably model path, probably
-            if( model_path[ 0 ] == '\0' ) {
-                strncpy( model_path, argv[ i ], sizeof( model_path ) );
-            } else { // probably extension, probably
-                strncpy( model_extension, argv[ i ], sizeof( model_extension ) );
-            }
-        }
-    }
+	for ( int i = 1; i < argc; ++i ) {
+		if ( argv[ i ] == NULL || argv[ i ][ 0 ] == '\0' ) {
+			continue;
+		}
 
-    if( model_path[ 0 ] == '\0' ) {
-        printf( "invalid path for model, aborting!\n" );
-        return EXIT_FAILURE;
-    }
+		if ( argv[ i ][ 0 ] != '-' ) {// probably model path, probably
+			if ( model_path[ 0 ] == '\0' ) {
+				strncpy( model_path, argv[ i ], sizeof( model_path ) );
+			} else {// probably extension, probably
+				strncpy( model_extension, argv[ i ], sizeof( model_extension ) );
+			}
+		}
+	}
 
-    if( scan_directory ) {
-        if( model_extension[ 0 ] == '\0' ) {
-            printf( "invalid extension for scan, aborting!\n" );
-            return EXIT_FAILURE;
-        }
+	if ( model_path[ 0 ] == '\0' ) {
+		printf( "invalid path for model, aborting!\n" );
+		return EXIT_FAILURE;
+	}
 
-        plScanDirectory( model_path, model_extension, TempModelLoad, false, NULL );
-        return EXIT_SUCCESS;
-    }
+	if ( scanDirectory ) {
+		if ( model_extension[ 0 ] == '\0' ) {
+			printf( "invalid extension for scan, aborting!\n" );
+			return EXIT_FAILURE;
+		}
 
-    mainWindow = plCreateWindow( WIDTH, HEIGHT, TITLE );
-    if( mainWindow ) {
-        PRINT_ERROR( "Failed to create window!\n%s\n", plGetError() );
-    }
+		plScanDirectory( model_path, model_extension, TempModelLoad, false, NULL );
+		return EXIT_SUCCESS;
+	}
 
-    plInitializeSubSystems( PL_SUBSYSTEM_GRAPHICS );
-    plSetGraphicsMode( PL_GFX_MODE_OPENGL );
+	mainWindow = plCreateWindow( WIDTH, HEIGHT, TITLE );
+	if ( mainWindow ) {
+		PRINT_ERROR( "Failed to create window!\n%s\n", plGetError() );
+	}
 
-    PLModel *model = plLoadModel( model_path );
-    if( model == NULL ) {
-        PRINT_ERROR( "Failed to load model \"%s\"!\n%s", model_path, plGetError() );
-    }
+	plInitializeSubSystems( PL_SUBSYSTEM_GRAPHICS );
+	plSetGraphicsMode( PL_GFX_MODE_OPENGL );
 
-    if( extract_model ) {
-        if( !plWriteModel( "output", model, PL_MODEL_OUTPUT_SMD ) ) {
-            PRINT_ERROR( "Failed to write model \"%s\"!\n%s\n", model->name, plGetError() );
-        }
-        return EXIT_SUCCESS;
-    }
+	PLModel *model = plLoadModel( model_path );
+	if ( model == NULL ) {
+		PRINT_ERROR( "Failed to load model \"%s\"!\n%s", model_path, plGetError() );
+	}
 
-    plSetClearColour( PLColour( 0, 0, 128, 255 ) );
+	if ( exportModel ) {
+		if ( !plWriteModel( "output", model, PL_MODEL_OUTPUT_SMD ) ) {
+			PRINT_ERROR( "Failed to write model \"%s\"!\n%s\n", model->name, plGetError() );
+		}
+		return EXIT_SUCCESS;
+	}
 
-    plSetupConsole( 1 );
-    plShowConsole( true );
-    plSetConsoleColour( 1, PLColour( 128, 0, 0, 128 ) );
+	plSetClearColour( PLColour( 0, 0, 128, 255 ) );
 
-    mainCamera = plCreateCamera();
-    if( mainCamera == NULL ) {
-        PRINT_ERROR( "Failed to create camera!\n" );
-    }
-    mainCamera->mode        = PL_CAMERA_MODE_PERSPECTIVE;
-    mainCamera->fov         = 75.0f;
-    mainCamera->position    = PLVector3( 0, 2, -50 );
-    mainCamera->viewport.w  = WIDTH;
-    mainCamera->viewport.h  = HEIGHT;
+	plSetupConsole( 1 );
+	plShowConsole( true );
+	plSetConsoleColour( 1, PLColour( 128, 0, 0, 128 ) );
+
+	mainCamera = plCreateCamera();
+	if ( mainCamera == NULL ) {
+		PRINT_ERROR( "Failed to create camera!\n" );
+	}
+	mainCamera->mode = PL_CAMERA_MODE_PERSPECTIVE;
+	mainCamera->fov = 75.0f;
+	mainCamera->position = PLVector3( 0, 2, -50 );
+	mainCamera->viewport.w = WIDTH;
+	mainCamera->viewport.h = HEIGHT;
 
 	plSetDepthBufferMode( PL_DEPTHBUFFER_ENABLE );
-    plSetCullMode( PL_CULL_NONE );
+	plSetCullMode( PL_CULL_NONE );
 
-    PLLight lights[ 4 ];
-    memset( &lights, 0, sizeof( PLLight ) * 4 );
-    lights[ 0 ].position     = PLVector3( 0, 0, 0 );
-    lights[ 0 ].colour       = plCreateColour4f( 1.5f, .5f, .5f, 128.f );
-    lights[ 0 ].type         = PL_LIGHT_TYPE_OMNI;
+	PLLight lights[ 4 ];
+	memset( &lights, 0, sizeof( PLLight ) * 4 );
+	lights[ 0 ].position = PLVector3( 0, 0, 0 );
+	lights[ 0 ].colour = plCreateColour4f( 1.5f, .5f, .5f, 128.f );
+	lights[ 0 ].type = PL_LIGHT_TYPE_OMNI;
 
-    /* compile shaders */
+	/* compile shaders */
 
-    const char *vertex_stage = {
-            "void main() {"
-            "   gl_Position = ftransform();"
-            "}"
-    };
+	const char *vertex_stage = {
+	        "void main() {"
+	        "   gl_Position = ftransform();"
+	        "}" };
 
-    const char *fragment_stage = {
-            "void main() {"
-            "   gl_FragColor = vec4(1,1,1,1);"
-            "}"
-    };
+	const char *fragment_stage = {
+	        "void main() {"
+	        "   gl_FragColor = vec4(1,1,1,1);"
+	        "}" };
 
-    PLShaderProgram *program = plCreateShaderProgram();
-    plRegisterShaderStageFromMemory( program, vertex_stage, strlen( vertex_stage ), PL_SHADER_TYPE_VERTEX );
-    plRegisterShaderStageFromMemory( program, fragment_stage, strlen( fragment_stage ), PL_SHADER_TYPE_FRAGMENT );
+	PLShaderProgram *program = plCreateShaderProgram();
+	plRegisterShaderStageFromMemory( program, vertex_stage, strlen( vertex_stage ), PL_SHADER_TYPE_VERTEX );
+	plRegisterShaderStageFromMemory( program, fragment_stage, strlen( fragment_stage ), PL_SHADER_TYPE_FRAGMENT );
 
-    plLinkShaderProgram(program);
-    plSetShaderProgram(program);
+	plLinkShaderProgram( program );
+	plSetShaderProgram( program );
 
-    /* done, now for main rendering loop! */
+	/* done, now for main rendering loop! */
 
-    while( plIsRunning() ) {
-        SDL_PumpEvents();
+	while ( plIsRunning() ) {
+		SDL_PumpEvents();
 
-        // input handlers start..
-        int xpos, ypos;
-        unsigned int state = SDL_GetMouseState( &xpos, &ypos );
+		// input handlers start..
+		int xpos, ypos;
+		unsigned int state = SDL_GetMouseState( &xpos, &ypos );
 
-        static PLVector3 object_angles = { 0, 0 };
-        if( useMouseLook ) {
-            object_angles = PLVector3( 0, 0, 0 );
+		static PLVector3 object_angles = { 0, 0 };
+		if ( useMouseLook ) {
+			object_angles = PLVector3( 0, 0, 0 );
 
-            double n_pos[ 2 ] = { xpos - CENTER_X, ypos - CENTER_Y };
-            mainCamera->angles.x += ( n_pos[ 0 ] / 10.f );
-            mainCamera->angles.y += ( n_pos[ 1 ] / 10.f );
-            mainCamera->angles.y = plClamp( -90, mainCamera->angles.y, 90 );
+			int n_pos[ 2 ] = { xpos - CENTER_X, ypos - CENTER_Y };
+			mainCamera->angles.x += ( float ) n_pos[ 0 ] / 10;
+			mainCamera->angles.y += ( float ) n_pos[ 1 ] / 10;
+			mainCamera->angles.y = plClamp( -90, mainCamera->angles.y, 90 );
 
-            SDL_WarpMouseInWindow( window, CENTER_X, CENTER_Y );
-        } else {
-            // Camera rotation
-            static double old_left_pos[ 2 ] = { 0, 0 };
-            if( state & SDL_BUTTON( SDL_BUTTON_LEFT ) ) {
-                double n_x_pos = xpos - old_left_pos[ 0 ];
-                double n_y_pos = ypos - old_left_pos[ 1 ];
-                object_angles.x += ( n_x_pos / 50.f );
-                object_angles.y += ( n_y_pos / 50.f );
-            } else {
-                old_left_pos[ 0 ] = xpos;
-                old_left_pos[ 1 ] = ypos;
-            }
+			SDL_WarpMouseInWindow( window, CENTER_X, CENTER_Y );
+		} else {
+			// Camera rotation
+			static double old_left_pos[ 2 ] = { 0, 0 };
+			if ( state & SDL_BUTTON( SDL_BUTTON_LEFT ) ) {
+				double n_x_pos = xpos - old_left_pos[ 0 ];
+				double n_y_pos = ypos - old_left_pos[ 1 ];
+				object_angles.x += ( float ) n_x_pos / 50;
+				object_angles.y += ( float ) n_y_pos / 50;
+			} else {
+				old_left_pos[ 0 ] = xpos;
+				old_left_pos[ 1 ] = ypos;
+			}
 
-            // Zoom in and out thing...
-            static double old_right_pos[ 2 ] = { 0, 0 };
-            if( state & SDL_BUTTON( SDL_BUTTON_RIGHT ) ) {
-                double n_y_pos = ypos - old_right_pos[ 1 ];
-                mainCamera->position.z += ( n_y_pos / 100.f );
-            } else {
-                old_right_pos[ 0 ] = xpos;
-                old_right_pos[ 1 ] = ypos;
-            }
+			// Zoom in and out thing...
+			static int old_right_pos[ 2 ] = { 0, 0 };
+			if ( state & SDL_BUTTON( SDL_BUTTON_RIGHT ) ) {
+				int n_y_pos = ypos - old_right_pos[ 1 ];
+				mainCamera->position.z += ( float ) n_y_pos / 100;
+			} else {
+				old_right_pos[ 0 ] = xpos;
+				old_right_pos[ 1 ] = ypos;
+			}
 
-            // panning thing
-            static double old_middle_pos[ 2 ] = { 0, 0 };
-            if( state & SDL_BUTTON( SDL_BUTTON_MIDDLE ) ) {
-                double n_x_pos = xpos - old_middle_pos[ 0 ];
-                double n_y_pos = ypos - old_middle_pos[ 1 ];
-                mainCamera->position.y += ( n_y_pos / 50.f );
-                mainCamera->position.x -= ( n_x_pos / 50.f );
-            } else {
-                old_middle_pos[ 0 ] = xpos;
-                old_middle_pos[ 1 ] = ypos;
-            }
-        }
-        // input handlers end...
+			// panning thing
+			static int old_middle_pos[ 2 ] = { 0, 0 };
+			if ( state & SDL_BUTTON( SDL_BUTTON_MIDDLE ) ) {
+				int n_x_pos = xpos - old_middle_pos[ 0 ];
+				int n_y_pos = ypos - old_middle_pos[ 1 ];
+				mainCamera->position.y += ( float ) n_y_pos / 50;
+				mainCamera->position.x -= ( float ) n_x_pos / 50;
+			} else {
+				old_middle_pos[ 0 ] = xpos;
+				old_middle_pos[ 1 ] = ypos;
+			}
+		}
+		// input handlers end...
 
-        ProcessKeyboard();
+		ProcessKeyboard();
 
-        plSetupCamera( mainCamera );
+		plSetupCamera( mainCamera );
 
-        plClearBuffers( PL_BUFFER_COLOUR | PL_BUFFER_DEPTH );
+		plClearBuffers( PL_BUFFER_COLOUR | PL_BUFFER_DEPTH );
 
-        plLoadIdentityMatrix();
-        plPushMatrix();
-        plRotateMatrix( object_angles.y, 1, 0, 0 );
-        plRotateMatrix( object_angles.x, 0, 1, 0 );
-        plRotateMatrix( object_angles.z, 0, 0, 1 );
+		plLoadIdentityMatrix();
+		plPushMatrix();
+		plRotateMatrix( object_angles.y, 1, 0, 0 );
+		plRotateMatrix( object_angles.x, 0, 1, 0 );
+		plRotateMatrix( object_angles.z, 0, 0, 1 );
 
-        switch( viewMode ) {
-        default: break;
+		switch ( viewMode ) {
+			default:
+				break;
 
-        case VIEW_MODE_LIT:
-        case VIEW_MODE_WEIGHTS:
-        case VIEW_MODE_WIREFRAME:
-            plDrawModel( model );
-            break;
+			case VIEW_MODE_LIT:
+			case VIEW_MODE_WEIGHTS:
+			case VIEW_MODE_WIREFRAME:
+				plDrawModel( model );
+				break;
 
-        case VIEW_MODE_SKELETON:
-            plDrawModelSkeleton( model );
-            break;
-        }
+			case VIEW_MODE_SKELETON:
+				plDrawModelSkeleton( model );
+				break;
+		}
 
-        plSwapWindow( mainWindow );
-    }
+		plSwapWindow( mainWindow );
+	}
 
-    plDestroyModel( model );
-    plDestroyCamera( mainCamera );
-    plDestroyWindow( mainWindow );
+	plDestroyModel( model );
+	plDestroyCamera( mainCamera );
+	plDestroyWindow( mainWindow );
 
-    plShutdown();
+	plShutdown();
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
