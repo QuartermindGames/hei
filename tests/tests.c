e1a74b3127bf595cc1f77dfe94cb15cf860a502d
Mark E Sowden <mark-sowden@outlook.com>
2021-01-10 13:09:21 +0000

    Made a start on getting tests in place.

---
+++ tests/tests.c
@@ -0,0 +1,111 @@
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
+
+#include <PL/platform.h>
+#include <PL/platform_console.h>
+
+enum {
+	TEST_RETURN_SUCCESS,
+	TEST_RETURN_FAILURE,
+	TEST_RETURN_FATAL,
+};
+
+#define FUNC_TEST( NAME )   uint8_t test_##NAME( void ) { printf( " Starting " #NAME );
+#define FUNC_TEST_END()     return TEST_RETURN_SUCCESS; }
+
+/*============================================================
+ * CONSOLE
+ ===========================================================*/
+
+static bool test_cmd_called = false;
+void CB_test_cmd( unsigned int argc, char **argv ) {
+	test_cmd_called = true;
+}
+
+FUNC_TEST( RegisterConsoleCommand )
+    plRegisterConsoleCommand( "test_cmd", CB_test_cmd, "testing" );
+    PLConsoleCommand *cmd = plGetConsoleCommand( "test_cmd" );
+    if ( cmd == NULL ) {
+	    printf( "test_cmd was not registered!\n" );
+	    return TEST_RETURN_FAILURE;
+    }
+    plParseConsoleString( "test_cmd" );
+    if ( !test_cmd_called ) {
+	    printf( "Failed to call \"test_cmd\" command!\n" );
+	    return TEST_RETURN_FAILURE;
+    }
+    test_cmd_called = false;
+FUNC_TEST_END()
+
+FUNC_TEST( GetConsoleCommands )
+    PLConsoleCommand **cmds;
+    size_t numCmds;
+    plGetConsoleCommands( &cmds, &numCmds );
+    if ( numCmds < 1 ) {
+	    printf( "Did not receive any commands!\n" );
+	    return TEST_RETURN_FAILURE;
+    }
+    if ( cmds == NULL ) {
+	    printf( "Returned null cmds list!\n" );
+	    return TEST_RETURN_FAILURE;
+    }
+    bool foundResult = false;
+    for ( unsigned int i = 0; i < numCmds; ++i ) {
+        const PLConsoleCommand *cmd = cmds[ i ];
+	    if ( strcmp( "test_cmd", cmd->cmd ) != 0 ) {
+		    continue;
+	    }
+	    foundResult = true;
+    }
+    if ( !foundResult ) {
+	    printf( "Failed to find test_cmd!\n" );
+	    return TEST_RETURN_FAILURE;
+    }
+FUNC_TEST_END()
+
+FUNC_TEST( GetConsoleCommand )
+    const PLConsoleCommand *cmd = plGetConsoleCommand( "test_cmd" );
+    if ( cmd == NULL ) {
+	    printf( "Failed to get test_cmd!\n" );
+	    return TEST_RETURN_FAILURE;
+    }
+FUNC_TEST_END()
+
+int main( int argc, char **argv ) {
+	printf( "Starting tests...\n" );
+
+#define CALL_FUNC_TEST( NAME ) \
+    { int ret = test_##NAME(); \
+		if ( ret != TEST_RETURN_SUCCESS ) { printf( "Failed on " #NAME "!\n"); \
+			if ( ret == TEST_RETURN_FATAL ) { return EXIT_FAILURE; } } }
+
+	CALL_FUNC_TEST( RegisterConsoleCommand )
+	CALL_FUNC_TEST( GetConsoleCommands )
+	CALL_FUNC_TEST( GetConsoleCommand )
+
+    return EXIT_SUCCESS;
+}
