7eaf87b6d60624e9ed2c9282997c2d7ca536ecb7
hogsy <markelswo@gmail.com>
2021-06-06 14:24:11 +0100

    Implementation of sdbm hashing algorithm.

--- plcore/include/plcore/pl_string.h
+++ plcore/include/plcore/pl_string.h
@@ -45,24 +45,24 @@ PL_INLINE static void plStripStringExtension(char *out, const char *in, unsigned
 }
 #endif
 
-char *pl_itoa(int val, char *buf, size_t len, int base);
+char *pl_itoa( int val, char *buf, size_t len, int base );
 
-char *pl_strtolower(char *s);
-char *pl_strntolower(char *s, size_t n);
-char *pl_strtoupper(char *s);
-char *pl_strntoupper(char *s, size_t n);
+char *pl_strtolower( char *s );
+char *pl_strntolower( char *s, size_t n );
+char *pl_strtoupper( char *s );
+char *pl_strntoupper( char *s, size_t n );
 
-char *pl_strcasestr(const char *s, const char *find);
+char *pl_strcasestr( const char *s, const char *find );
 
-int pl_strcasecmp(const char *s1, const char *s2);
-int pl_strncasecmp(const char *s1, const char *s2, size_t n);
+int pl_strcasecmp( const char *s1, const char *s2 );
+int pl_strncasecmp( const char *s1, const char *s2, size_t n );
 
-int pl_strisalpha(const char *s);
-int pl_strnisalpha(const char *s, unsigned int n);
-int pl_strisalnum(const char *s);
-int pl_strnisalnum(const char *s, unsigned int n);
-int pl_strisdigit(const char *s);
-int pl_strnisdigit(const char *s, unsigned int n);
+int pl_strisalpha( const char *s );
+int pl_strnisalpha( const char *s, unsigned int n );
+int pl_strisalnum( const char *s );
+int pl_strnisalnum( const char *s, unsigned int n );
+int pl_strisdigit( const char *s );
+int pl_strnisdigit( const char *s, unsigned int n );
 
 int pl_vscprintf( const char *format, va_list pArgs );
 
@@ -70,3 +70,15 @@ unsigned int pl_strcnt( const char *s, char c );
 unsigned int pl_strncnt( const char *s, char c, unsigned int n );
 
 char *PlStrInsert( const char *string, char **buf, size_t *bufSize, size_t *maxBufSize );
+
+/**
+ * http://www.cse.yorku.ca/~oz/hash.html#sdbm
+ */
+static PL_INLINE unsigned long PlStrHash_sdbm( const unsigned char *str ) {
+	unsigned long hash = 0;
+	while( int c = *str++ ) {
+		hash = c + ( hash << 6 ) + ( hash << 16 ) - hash;
+	}
+
+	return hash;
+}
