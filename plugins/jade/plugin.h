c59bc9c12244ee7df7898eb690a0bd849bbb8ca4
Mark Sowden <markelswo@gmail.com>
2021-11-16 18:38:07 +0000

    Remove Jade crap.

--- plugins/jade/plugin.h
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2020 Mark E Sowden <markelswo@gmail.com> */
-
-#pragma once
-
-#define PL_COMPILE_PLUGIN 1
-#include <PL/pl_plugin_interface.h>
-
-extern const PLPluginExportTable *gInterface;
