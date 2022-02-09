559d918b7dd95425b527f78bea4ff80d1dc14413
Mark Sowden <markelswo@gmail.com>
2022-02-09 21:59:52 +0000

    Why can't things ever be simple?

--- plcore/pl_filesystem.c
+++ plcore/pl_filesystem.c
@@ -50,7 +50,11 @@
 static PLFileOffset pl_ftell( FILE *file ) {
 #ifdef PL_FILESYSTEM_64
 #	if ( PL_SYSTEM_OS == PL_SYSTEM_OS_WINDOWS )
+#		if defined( _MSC_VER )
+	return _ftelli64( file );
+#		else
 	return ftello64( file );
+#		endif
 #	else
 	return ftello( file );
 #	endif
@@ -62,7 +66,11 @@ static PLFileOffset pl_ftell( FILE *file ) {
 static int pl_fseek( FILE *file, PLFileOffset off, int wence ) {
 #ifdef PL_FILESYSTEM_64
 #	if ( PL_SYSTEM_OS == PL_SYSTEM_OS_WINDOWS )
+#		if defined( _MSC_VER )
+	return _fseeki64( file, off, wence );
+#		else
 	return fseeko64( file, off, wence );
+#		endif
 #	else
 	return fseeko( file, off, wence );
 #	endif
