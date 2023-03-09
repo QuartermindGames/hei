eb1ce0669ff25bdc675d8445589832bb0ba1a1e8
hogsy <hogsy@oldtimes-software.com>
2023-03-09 23:30:02 +0000

    tiny bit more...

--- plmodel/plm_format_ply.c
+++ plmodel/plm_format_ply.c
@@ -46,12 +46,36 @@ typedef struct PLYElement {
 	unsigned int numProperties;                  // number of properties for this element
 } PLYElement;
 
+unsigned int PropertyTypeForToken( const char *token ) {
+	if ( strcmp( token, "char" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_CHAR;
+	} else if ( strcmp( token, "uchar" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_UCHAR;
+	} else if ( strcmp( token, "short" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_SHORT;
+	} else if ( strcmp( token, "ushort" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_USHORT;
+	} else if ( strcmp( token, "int" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_INT;
+	} else if ( strcmp( token, "uint" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_UINT;
+	} else if ( strcmp( token, "float" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_FLOAT;
+	} else if ( strcmp( token, "double" ) == 0 ) {
+		return PLY_PROPERTY_TYPE_DOUBLE;
+	} else {
+		return PLY_PROPERTY_TYPE_INVALID;
+	}
+}
+
 static PLMModel *ParsePly( const char *buf ) {
 	const char *p = buf;
 
 	PLYElement elements[ PLY_MAX_ELEMENTS ];
 	PL_ZERO( elements, sizeof( PLYElement ) * PLY_MAX_ELEMENTS );
 	int numElements = -1;
+
+	PLYElement *element = NULL;
 
 	// this reads in the header block *after* the format line
 	while ( *p != '\0' ) {
@@ -60,24 +84,26 @@ static PLMModel *ParsePly( const char *buf ) {
 			break;
 		}
 
-		if ( strcmp( token, "comment" ) == 0 ) {
-			PlSkipLine( &p );
-			continue;
-		} else if ( strcmp( token, "element" ) == 0 ) {
+		if ( strcmp( token, "element" ) == 0 ) {
 			numElements++;
 			PlParseToken( &p, elements[ numElements ].name, sizeof( PLYName ) );
 			elements[ numElements ].num = PlParseInteger( &p, NULL );
-			continue;
+			element = &elements[ numElements ];
 		} else if ( strcmp( token, "property" ) == 0 ) {
-			if ( numElements == -1 ) {
+			if ( element == NULL ) {
 				PlReportErrorF( PL_RESULT_FILEERR, "property with no element" );
 				PlSkipLine( &p );
 				continue;
 			}
+
+			element->properties[ element->numProperties ].type = PropertyTypeForToken( PlParseToken( &p, token, sizeof( token ) ) );
+			snprintf( element->properties[ element->numProperties ].name, sizeof( PLYName ), "%s", PlParseToken( &p, token, sizeof( token ) ) );
 		} else if ( strcmp( token, "end_header" ) == 0 ) {
 			PlSkipLine( &p );
 			break;
 		}
+
+		PlSkipLine( &p );
 	}
 
 	if ( *p == '\0' ) {
