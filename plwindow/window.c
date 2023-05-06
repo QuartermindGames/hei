a39a25b74244bc338a4b9fd4f000ab9467bafe24
hogsy <markelswo@gmail.com>
2023-05-06 21:17:06 +0100

    Basic window creation with X.

--- plwindow/window.c
+++ plwindow/window.c
@@ -4,6 +4,16 @@
 
 #include "window_private.h"
 
+#if defined( __linux__ )
+
+static Display *display = NULL;
+
+typedef struct HeiNativeWindow {
+	Window win;
+} HeiNativeWindow;
+
+#endif
+
 /////////////////////////////////////////////////////////////////////////////////////
 // Error Management
 
@@ -94,6 +104,39 @@ PLWWindow *PlwCreateWindow( const char *title, int width, int height ) {
 
 	return window;
 
+#elif defined( __linux__ )
+
+	if ( display == NULL ) {
+		display = XOpenDisplay( NULL );
+		if ( display == NULL ) {
+			PlwSetError( PLW_ERROR_WINDOW_CREATE, "failed to open display" );
+			return NULL;
+		}
+	}
+
+	int screen = XDefaultScreen( display );
+	Window win = XCreateSimpleWindow( display, XRootWindow( display, screen ),
+	                                  0, 0,
+	                                  width, height,
+	                                  1,
+	                                  XBlackPixel( display, screen ),
+	                                  XWhitePixel( display, screen ) );
+	if ( win == None ) {
+		//todo: make this more verbose...
+		PlwSetError( PLW_ERROR_WINDOW_CREATE, "failed to create window" );
+		return NULL;
+	}
+
+	XStoreName( display, win, title );
+
+	XMapWindow( display, win );
+	XFlush( display );
+
+	PLWWindow *window = PL_NEW( PLWWindow );
+	window->nativeWindow = PL_NEW( HeiNativeWindow );
+	( ( HeiNativeWindow * ) window->nativeWindow )->win = win;
+	return window;
+
 #else
 
 #	error "CreateWindow unsupported!"
@@ -132,6 +175,17 @@ void PlGetWindowPosition( PLWWindow *window, int *x, int *y ) {
 
 	*x = position.left;
 	*y = position.top;
+
+#elif defined( __linux__ )
+
+	XWindowAttributes attributes;
+	if ( XGetWindowAttributes( display, ( Window ) window->nativeWindow, &attributes ) != None ) {
+		//todo: make this more verbose...
+		PlwSetError( PLW_ERROR_WINDOW_GET, "failed to fetch window attributes" );
+		return;
+	}
+	*x = attributes.x;
+	*y = attributes.y;
 
 #else
 
--- plwindow/window_private.h
+++ plwindow/window_private.h
@@ -9,6 +9,8 @@
 #if defined( _WIN32 )
 #	define WIN32_LEAN_AND_MEAN 1
 #	include <Windows.h>
+#elif defined( __linux__ )
+#	include <X11/Xlib.h>
 #endif
 
 typedef struct PLWWindow {
